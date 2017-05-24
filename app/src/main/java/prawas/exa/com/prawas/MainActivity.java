1b6e9dbdef0c2bd349b26c47a94aa8b129f06c15
Shirish <developer.shirish@gmail.com>
2017-05-24 12:05:33 +0530

    upload layout

--- app/src/main/java/prawas/exa/com/prawas/MainActivity.java
+++ app/src/main/java/prawas/exa/com/prawas/MainActivity.java
@@ -15,6 +15,7 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
